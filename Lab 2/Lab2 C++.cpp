33f9e078edaacf44dfe88b0a31b423dec1ae9152
glebchik200 <uktrmama@gmail.com>
2025-12-18 12:29:41 +0300

    Create Lab2 C++.cpp

---
+++ Lab 2/Lab2 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void first_func();
+void second_func();
+void third_func();
+
+int main()
+{
+	//first_func();
+	//second_func();
+	third_func();
+
+
+	return 0;
+}
